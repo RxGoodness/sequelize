4f65817ea8cab305a4b8b39ea9d27b7a1bf7b3a7
rxgoodness <adedokuntoyyib@gmail.com>
2022-07-06 07:45:29 -0700

    start

---
+++ try1.sql
@@ -0,0 +1,72 @@
+-- SELECT * FROM users WHERE location='New York' AND last_name='smith';
+-- delete from users where id=6--
+-- select * from users 
+-- update users set email='ade2g@gmail.com' where id=1;
+-- ALTER TABLE users ADD age VARCHAR(3);
+-- ALTER TABLE users MODIFY COLUMN age INT(3);
+-- select * from users ORDER BY last_name asc;
+-- select concat(first_name, ' ', last_name) AS 'Name' , dept FROM users;
+-- SELECT DISTINCT location FROM users;
+-- SELECT * FROM users WHERE age BETWEEN 15 AND 30;
+-- SELECT * FROM users WHERE dept LIKE 'd%';
+-- SELECT * FROM users WHERE dept LIKE '%t';
+-- SELECT * FROM users WHERE dept LIKE '%op%';
+-- SELECT * FROM users WHERE dept NOT LIKE '%op%';
+-- SELECT * FROM users WHERE dept IN('design', 'sales');
+-- CREATE INDEX LIndex on users(location);
+-- DROP INDEX LIndex on users;
+-- create table posts(
+-- id int auto_increment,
+-- user_id int,
+-- title varchar(100),
+-- body text,
+-- publish_date datetime default current_timestamp,
+-- primary key(id),
+-- foreign key(user_id) references users(id)
+-- );
+-- select * from posts;
+-- INSERT INTO posts(user_id, title, body) VALUES (1, 'Post One', 'This is post one'),(3, 'Post Two', 'This is post two'),(1, 'Post Three', 'This is post three'),(2, 'Post Four', 'This is post four'),(5, 'Post Five', 'This is post five'),(4, 'Post Six', 'This is post six'),(2, 'Post Seven', 'This is post seven'),(1, 'Post Eight', 'This is post eight'),(3, 'Post Nine', 'This is post none'),(4, 'Post Ten', 'This is post ten');
+-- select
+-- users.first_name,
+-- users.last_name,
+-- posts.title,
+-- posts.publish_date
+-- from users
+-- inner join posts
+-- on users.id = posts.user_id
+-- order by posts.title;
+-- create table comments(
+-- id int auto_increment,
+-- post_id int, 
+-- user_id int,
+-- body text,
+-- publish_date datetime default current_timestamp,
+-- primary key(id),
+-- foreign key(user_id) references users(id),
+-- foreign key(post_id) references posts(id)
+-- );
+-- INSERT INTO comments(post_id, user_id, body) VALUES (1, 3, 'This is comment one'),(2, 1, 'This is comment two'),(5, 3, 'This is comment three'),(2, 4, 'This is comment four'),(1, 2, 'This is comment five'),(3, 1, 'This is comment six'),(3, 2, 'This is comment six'),(5, 4, 'This is comment seven'),(2, 3, 'This is comment seven');
+-- select * from comments;
+-- select
+-- comments.body,
+-- posts.title
+-- from comments
+-- left join posts on posts.id = comments.id
+-- order by posts.title;
+-- select
+-- comments.body,
+-- posts.title,
+-- users.first_name,
+-- users.last_name
+-- from comments
+-- inner join posts on posts.id = comments.post_id
+-- inner join users on users.id = comments.user_id
+-- order by posts.title;
+-- select count(id) from users;
+-- select max(age) from users;
+-- select min(age) from users;
+-- select sum(age) from users;
+-- select ucase(first_name), lcase(last_name) from users;
+select location, count(location) from users where location= 'New York'group by location;
+
+
