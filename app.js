cca09bc9398cc42439bd4856c7601812786031ff
rxgoodness <adedokuntoyyib@gmail.com>
2022-07-10 07:08:26 -0700

    sequelize

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+
+// app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '@Ata287693',
+    database: 'rxgoodness'
+});
+db.connect();
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// }
+// );
+app.get('/users', (req, res) => {
+    const sql = `SELECT * FROM users`
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+}
+);
+
+
+
+app.listen(4110, () => {
+    console.log('Server is running on port 4110');
+}   
+);
